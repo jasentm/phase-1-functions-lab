5da414ce74f0e88bfdc2b2f49e6731fa6f479f55
Jasen <jasen.miyamoto@gmail.com>
2024-02-19 19:09:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(address) {
+    const distanceInBlocks = address - 42
+    if (distanceInBlocks < 0) {
+        const positiveDistance = (distanceInBlocks * -1);
+        return positiveDistance;
+    } else {
+        return distanceInBlocks;
+    }
+}
+
+function distanceFromHqInFeet(address) {
+    return distanceFromHqInBlocks(address) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * .02);
+    } else if (distanceTravelledInFeet(start, destination) <= 2499) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) >= 2500) {
+        return 'cannot travel that far';
+    }
+}
